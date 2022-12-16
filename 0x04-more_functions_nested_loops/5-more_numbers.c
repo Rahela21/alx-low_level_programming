80b25291fa4c4f7a93f8adc4a1549b73ac0a31ae
Rahela21 <rahelberhe216@gmai.com>
2022-12-15 17:18:36 -0800

    seventh commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,28 +1,24 @@
-#include "holberton.h"
-#include <stdio.h>
-
+nclude "main.h"
 /**
- * more_numbers - Print numbers between 0 to 14 incl.
+ * more_numbers - Print numbers to 14
  *
- * Return: Void.
+ * Return: returns nothing
  */
 void more_numbers(void)
 {
-	int i;
-	int x;
+	int number1, number2;
 
-	for (x = 0; x < 10; x++)
+	for (number1 = 0; number1 <10; number1++)
 	{
-		i = 0;
-		while (i <= 14)
+		(number2 =0; number2 <= 14; number2++)
+
 		{
-			if (i >= 10)
+			if (number2 > 9)
 			{
-				_putchar(i / 10 + '0');
+				_putchar((number2 / 10) + '0'):
 			}
-			_putchar(i % 10 + '0');
-			i++;
+			_putchar ((number2 % 10) +'0');
 		}
-		_putchar('\n');
+		_putchar(10);
 	}
 }
