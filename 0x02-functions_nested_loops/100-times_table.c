df3099821303a7a6abdfe9993b3a87d2597670be
Rahela21 <rahelberhe216@gmai.com>
2022-12-18 01:45:34 -0800

    ninth commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,34 +1,50 @@
-#include "main.h"
+nclude "main.h"
 
 /**
- * print_times_table - Entery point
- * @n: integer
- * Description: It prints n times table starting from 0
- *
- * Return: void
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
  */
 void print_times_table(int n)
-	int col, row, product;
-
-	if (n < 15 && n >= 0)
 {
-	for (row =0; row <=n; row++)
+	int i, j, k;
+
+	if (n >= 0 && n <= 15)
 	{
-		for (col = 0; col <= n; col++)
+		for (i = 0; i <= n; i++)
 		{
-			product = row * col;
-			if (col == 0)
-				_putchar('0');
-			else if (product < 10)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(product % 10 + '0');
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				}
+				else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				}
+				else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
 			}
-			else if (product < 100 && product >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-			
+			_putchar('\n');
+		}
+	}
+}
