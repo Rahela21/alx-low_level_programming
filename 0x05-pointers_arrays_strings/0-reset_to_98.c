778e8210308a811ea6836d11bf45b136c98e1b58
Rahela21 <rahelberhe216@gmai.com>
2022-12-22 20:17:11 -0800

    changing the value of n using pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * reset_to_98 - takes a pointer to an int as parameter and
@@ -9,10 +8,4 @@
 void reset_to_98(int *n)
 {
 	*n = 98;
-
-	n = 402;
-	printf("n=%d\n", n);
-	reset_to_98(&n);
-	printf("n=%d\n", n);
-	return (0);
 }
