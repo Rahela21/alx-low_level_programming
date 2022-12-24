560560ef20f7a8d448a40301f8f33b4d9d1b879d
Rahela21 <rahelberhe216@gmail.com>
2022-12-23 19:19:08 -0800

    tenth commit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,7 +11,7 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 void print_number(int n);
-int _putchar(char c);
+char *rot13(char *str);
 void print_buffer(char *b, int size);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
